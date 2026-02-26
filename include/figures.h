cde7468c78705e2bec5e64a22f0d94cdb8a91895
Simão Oliveira <simaooliveira106@gmail.com>
2026-02-26 23:31:52 +0000

    Small fix to includes

--- include/figures.h
+++ include/figures.h
@@ -7,6 +7,6 @@ void generateBox(float length, int divisions, list<string>& vertices);
 void generatePlane(float length, int divisions, list<string>& vertices);
 void generateSphere(float radius, int slices, int stacks, list<string>& vertices);
 void generateCone(float radius, float height, int slices, int stacks, list<string>& vertices);
-void generateCylinder(float radius, float height, int slices, list<string>& vertices);
+void generateCylinder(float radius, float height, int slices, int stacks, list<string>& vertices);
 void generateIcosphere(float radius, int subdivisions, list<string>& vertices);
 void generateTorus(float ringRadius, float pipeRadius, int slices, int stacks, list<string>& vertices);
